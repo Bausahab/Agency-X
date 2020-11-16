7b3918eca994fd21bfad376d73b15331d10b6f05
Manish Raj Singh <36834313+Bausahab@users.noreply.github.com>
2020-11-16 19:29:50 +0530

    UCwhouse.cs

--- UI/UCwhouse.cs
+++ UI/UCwhouse.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//Manish Raj Singh@2020
 namespace UI
 {
     public partial class UCwhouse : UserControl
